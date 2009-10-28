9a32f246c5ad295a231557e4257707349464c898
Adrian Webb <adrian@collectivecolors.com>
2009-10-28 17:43:06 -0400

    Made some form modifications to the add/edit rule form.
    1. Added jquery hiding of unused form elements.
    2. Added add/edit rule form validation logic.
    3. Added add/edit rule form submit logic.
    4. Apparently newlines or something got converted in every file. ( that freakin sucks )

--- uc_member_pricing.js
+++ uc_member_pricing.js
@@ -1,4 +1,30 @@
 
 Drupal.behaviors.uc_member_pricing_rule_edit_toggle = function( context ) {
-  
+	
+	// Toggle form elements based upon rule type radio button selection.
+	var toggle_form_type = function( ) {
+		var value = $('input[name="rule_type"]:checked').val();
+		
+		switch ( value ) {
+		case '0': // User based rule.
+			$( '#rule_role-wrapper' ).hide( );
+			$( '#rule_role' ).attr( 'disabled', 'disabled' );
+			
+			$( '#rule_user-wrapper' ).show( );
+			$( '#rule_user' ).removeAttr( 'disabled' );
+			break;
+			
+		case '1': // Role based rule.
+			$( '#rule_user-wrapper' ).hide( );
+			$( '#rule_user' ).attr( 'disabled', 'disabled' );
+			
+			$( '#rule_role-wrapper' ).show( );
+			$( '#rule_role' ).removeAttr( 'disabled' );
+			break;		
+		}		
+	}
+	
+	// Start process.
+	$('input[name="rule_type"]').change( toggle_form_type );
+	toggle_form_type( );
 }
